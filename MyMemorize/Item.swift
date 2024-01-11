666c3d876f276c48c6aa8ec7bf5186293d8ea8ec
Maja <md.szlesinska@gmail.com>
2024-01-11 15:59:00 +0100

    Initial Commit

---
+++ MyMemorize/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyMemorize
+//
+//  Created by Maja on 11/01/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
